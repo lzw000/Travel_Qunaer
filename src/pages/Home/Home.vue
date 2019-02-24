88fbc48303722d6a12eb302ed8baada87b541e7d
zemicro <1206678340@qq.com>
2019-02-24 14:21:05 +0800

    add header

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-	Helloworld
-  </div>
+ <div>
+  <home-header></home-header>
+ </div>
 </template>
 
 <script>
+	import HomeHeader from './components/Header'
 export default {
   name: 'Home',
-
+  components:{
+  	HomeHeader
+  }
 }
 </script>
 
