46185d7be07019956e765ebcaaae8ebab2a943c3
zemicro <1206678340@qq.com>
2019-02-25 11:31:10 +0800

    city router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/Home/Home'
+import City from '@/pages/City/City'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
+  routes: [{
       path: '/',
       name: 'Home',
       component: Home
-    }
-  ]
+    },{
+      path: '/City',
+      name: 'City',
+      component: City
+    }]
 })
